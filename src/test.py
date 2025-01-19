b1f2a69561cb60a0fc270c1ca32d09e2ddd642d6
Нагорный Дмитрий Александрович <nagornyyda1@edu.mos.ru>
2025-01-19 19:38:32 +0300

    init commit

---
+++ src/test.py
@@ -0,0 +1,91 @@
+import asyncio
+import time
+import json
+from typing import List, Union, Dict
+import openai
+
+HOST = '1'
+PORT = 8000
+PROTOCOL = 'http'
+
+BASE_URL = f"{PROTOCOL}://{HOST}:{PORT}/v1/"
+MODEL = f"GameScribes/Mistral-Nemo-AWQ"
+API_KEY = "<KEY>"
+
+
+# Функция для выполнения одного асинхронного запроса к модели
+async def make_request(client, model: str, prompt: str, max_tokens: int = 2048) -> Dict:
+    start_time = time.time()
+    try:
+        response = await asyncio.to_thread(
+            client.chat.completions.create,
+            model=model,
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=max_tokens
+        )
+        end_time = time.time()
+        output_text = response.choices[0].message.content
+        token_count = len(output_text.split())
+        response_time = end_time - start_time
+        return {
+            "response": output_text,
+            "token_count": token_count,
+            "response_time": response_time,
+        }
+    except Exception as e:
+        return {"error": str(e)}
+
+# Основной метод для тестирования скорости генерации токенов
+async def test_generation_speed(
+    client, model: str, queries: Union[List[str], str], parallel_requests: int
+) -> Dict[int, Dict]:
+    if isinstance(queries, str):
+        queries = [queries]
+
+    results = {}
+
+    # Ограничиваем число параллельных запросов с помощью семафора
+    semaphore = asyncio.Semaphore(parallel_requests)
+
+    async def sem_task(task):
+        async with semaphore:
+            return await task
+
+    tasks = [
+        sem_task(make_request(client, model, query))
+        for query in queries
+    ]
+
+    responses = await asyncio.gather(*tasks, return_exceptions=True)
+
+    # Сохраняем результаты
+    for idx, response in enumerate(responses):
+        if isinstance(response, Exception):
+            results[idx] = {"error": str(response)}
+        else:
+            results[idx] = response
+
+    return results
+
+
+# Пример использования
+if __name__ == "__main__":
+
+    test_queries = "Привет!"
+    parallel_requests = 5
+
+    openai.api_key = API_KEY
+    openai.api_base = BASE_URL
+    client = openai
+
+    # Запуск теста
+    results = asyncio.run(test_generation_speed(client, MODEL, test_queries, parallel_requests))
+
+    with open('data.json', 'w') as f:
+        json.dump(results, f)
+
+    # Вывод результатов
+    for idx, result in results.items():
+        print(f"Request {idx}: {result}")
